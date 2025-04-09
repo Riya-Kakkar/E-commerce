7fbd87276af318e4ffce1871be761cccfbece726
Riya-Kakkar <riyukakkar@gmail.com>
2025-04-09 15:11:32 +0530

    done with jwt

--- src/main/java/com/E_commerce/Model/AuthRequest.java
+++ src/main/java/com/E_commerce/Model/AuthRequest.java
@@ -1,9 +1,16 @@
 package com.E_commerce.Model;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AuthRequest {
+    @Column(unique = true)
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@(gmail|yahoo|outlook)\\.com$" , message = "Invalid Email !! ")
     private String email;
+
+    @NotBlank(message = "Password cannot be empty!!")
     private String password;
 }
