fa15fd8b87f6a6611811189cb10cd4b9c84e14a5
Riya-Kakkar <riyukakkar@gmail.com>
2025-04-30 10:14:44 +0530

    updated with postman

--- src/main/java/com/E_commerce/Service/AdminService.java
+++ src/main/java/com/E_commerce/Service/AdminService.java
@@ -1,11 +1,15 @@
 package com.E_commerce.Service;
 
+import com.E_commerce.Entity.User;
 import com.E_commerce.Model.AdminDashboardDTO;
 import com.E_commerce.Repository.OrderRepository;
 import com.E_commerce.Repository.ProductRepository;
 import com.E_commerce.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AdminService {
@@ -16,6 +20,7 @@ public class AdminService {
     @Autowired
     private OrderRepository orderRepository;
 
+    private final List<String> allowedRoles = List.of("ROLE_ADMIN", "ROLE_CUSTOMER", "ROLE_SELLER");
 
     public AdminDashboardDTO getDashboardStats() {
         long totalUsers = userRepository.count();
@@ -24,5 +29,18 @@ public class AdminService {
         double totalRevenue = orderRepository.calculateTotalRevenue();
 
         return new AdminDashboardDTO(totalUsers, totalProducts, totalOrders, totalRevenue);
+    }
+
+    public User updateUserRole(int userId, String role) {
+
+        if (!allowedRoles.contains(role.toUpperCase())) {
+            throw new IllegalArgumentException("Invalid role: " + role);
+        }
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        user.setRole(role.toUpperCase());
+        return userRepository.save(user);
     }
 }
